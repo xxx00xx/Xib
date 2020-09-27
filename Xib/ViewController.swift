9bfda07d91a7110f67d15962c7db3d0d1ba5f731
Shotaro Furuta <zoxx31syg1y999@gmail.com>
2020-09-28 01:44:08 +0900

    Initial Commit

---
+++ Xib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xib
+//
+//  Created by 古田翔太郎 on 2020/09/28.
+//  Copyright © 2020 古田翔太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
